3f21097724ae0d5a6daea108878e5ef07cf2a5b8
Jack Quinoa <jquinoa208@gmail.com>
2020-06-07 20:49:23 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,28 @@
 class Post 
   
+  attr_accessor :title 
+  @@all = []
+  
   def initialize(title)
     @title = title
     @@all << self 
   end
   
+  def self.all 
+    
+  end
+  
+  def title 
+    
+  end
+  
+  def author 
+    
+  end
+  
+  def author_name
+    
+  end
   
   
 end
